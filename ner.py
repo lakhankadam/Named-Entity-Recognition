afb03ace51dc67c96ebffcf1e4ba05c36e998271
lakhankadam1 <lakhankadam1@gmail.com>
2021-08-14 18:50:58 +0530

    Plotting model

--- ner.py
+++ ner.py
@@ -8,7 +8,7 @@ def run():
     results = pd.DataFrame()
     train_tokens, val_tokens, test_tokens, train_tags, val_tags, test_tags = split_train_test_val()
     model_bilstm_lstm = get_lstm_model()
-    # plot_model(model_bilstm_lstm)
+    plot_model(model_bilstm_lstm)
     results['with_add_lstm'] = train_model(train_tokens, np.array(train_tags), model_bilstm_lstm)
 
 if __name__ == '__main__':
